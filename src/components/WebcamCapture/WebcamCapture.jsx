b2c77ab5a7e3c2a4e10b7465a5dcb706cb9ab165
Adam Bertrand <ajbertra91@gmail.com>
2022-10-13 13:18:19 -0400

    adds switch camera button

--- src/components/WebcamCapture/WebcamCapture.jsx
+++ src/components/WebcamCapture/WebcamCapture.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Webcam from "react-webcam";
+import { MdCameraswitch } from 'react-icons/md';
 
 const videoConstraints = {
   width: 1280,
   height: 720,
-  facingMode: "user",
+  facingMode: "user", // || { exact: "environment" }
   mirror: true
 };
 
 export const WebcamCapture = ({postCaptureCb, ...props}) => {
   const webcamRef = React.useRef(null);
   const [image, setImage] = useState('');
+  const [facingMode, setFacingMode] = useState('user');
   const [isImageCaptured, setIsImageCaptured] = useState(false);
-  const [height, setHeight] = useState();
-  const [width, setWidth] = useState();
+  const [height] = useState(450);
+  const [width] = useState(800);
 
   const capture = React.useCallback(
     () => {
@@ -23,16 +25,19 @@ export const WebcamCapture = ({postCaptureCb, ...props}) => {
     });
 
   useEffect(() => {
-    setWidth(800);
-    setHeight(450);
-  }, []);
-
-  useEffect(() => {
     if (isImageCaptured) {
       postCaptureCb();
       setIsImageCaptured(false);
     }
-  }, [isImageCaptured]);
+  }, [isImageCaptured, postCaptureCb]);
+
+  const toggleCamera = () => {
+    if (facingMode &&  facingMode.exact) {
+      setFacingMode('user');
+    } else {
+      setFacingMode({exact: 'environment'})
+    }
+  }
 
   return (
     <div className="webcam-container">
@@ -50,16 +55,21 @@ export const WebcamCapture = ({postCaptureCb, ...props}) => {
             </>
           : <img src={image} alt="" id="image" />
         }
-
-        <>
-          {image !== ''
-            ? <button onClick={(e) => { e.preventDefault(); setImage(''); }} className="webcam-btn"
-                >Retake Image</button>
-            : <button onClick={(e) => { e.preventDefault(); capture(); }} className="webcam-btn"
-                >Capture</button>
-          }
-        </>
       </div>
+
+      <>
+        {isImageCaptured
+          ? <button onClick={(e) => { e.preventDefault(); setImage(''); }} className="webcam-btn"
+              >Retake Image</button>
+          : <button onClick={(e) => { e.preventDefault(); capture(); }} className="webcam-btn"
+              >Capture</button>
+        }
+        <button onClick={(e) => {
+          e.preventDefault();
+          toggleCamera();
+        }} className="switch-btn"
+        ><MdCameraswitch /></button>
+      </>
     </div>
   );
 };
